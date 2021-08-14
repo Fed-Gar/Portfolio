5056fd0a3d4c96f7d6d53b0f2b4875b2e1c3017d
Fed-Gar <garcia.fed3rico@gmail.com>
2021-08-14 18:32:14 -0300

    NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import clsx from 'clsx';
-
-import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import MenuOpenOutlinedIcon from '@material-ui/icons/MenuOpenOutlined';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Link from '@material-ui/core/Link';
+
 import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
 import ComputerOutlinedIcon from '@material-ui/icons/ComputerOutlined';
 import MailIcon from '@material-ui/icons/Mail';
 import WorkIcon from '@material-ui/icons/Work';
 
 import { useStyles } from './styles';
-import { useTheme } from '@material-ui/core/styles';
 
 const list = [
     {
@@ -43,59 +32,30 @@ const list = [
 
 export default function NavBar() {
   const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = useState(false);
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
+  const preventDefault = (event) => event.preventDefault();
+  
   return (
-    <div className={classes.root}>
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
-            <MenuOpenOutlinedIcon />
-          </IconButton>
+    <>
+      <AppBar className={ classes.appBar }>
+        <Toolbar className={ classes.toolBar }>
+          {
+            list.map(item => {
+              return (
+                <Link 
+                  key={ item.title } 
+                  className={ classes.link }
+                  href="#" 
+                  onClick={ preventDefault }
+                >
+                  { item.icon }
+                </Link>
+              );
+            })
+          }
         </Toolbar>
       </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={ { paper: classes.drawerPaper, } }
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          {list.map((item) => (
-            <ListItem button key={ item.title }>
-              <ListItemIcon> { item.icon } </ListItemIcon>
-              <ListItemText primary={ item.title }/>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-    </div>
+      <div className={ classes.offset }></div>
+    </>
   );
 };
--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,60 +1,18 @@
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
-const drawerWidth = 180;
-
 export const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
   appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerHeader: {
-    display: 'flex',
+    flexDirection: 'row',
     alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
+    backgroundColor: theme.palette.background.default,
   },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
+  toolBar: {
+    width: '50%',
+    justifyContent: 'space-between',
   },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
+  link: {
+    color: theme.palette.text.primary,
   },
+  offfset: theme.mixins.toolbar,
 }));
