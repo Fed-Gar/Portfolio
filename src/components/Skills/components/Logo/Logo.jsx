a78acf98268839bfe891435b5c96d5c9dbaf0141
Fed-Gar <garcia.fed3rico@gmail.com>
2021-08-14 18:31:55 -0300

    Logo

---
+++ src/components/Skills/components/Logo/Logo.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { 
+	Box,
+	Grid,
+ } from '@material-ui/core';
+
+import { useStyles } from './styles';
+
+export default function Logo() {
+  const classes = useStyles();
+	
+	return (
+		<Box>
+			<Grid>
+				Logo
+			</Grid>
+		</Box>
+	);  
+};
