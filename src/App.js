2f61809652e6ea336e1e8ffe09b8fb186131c5a0
Fed-Gar <garcia.fed3rico@gmail.com>
2021-08-14 18:32:43 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
-// import NavBar from '';
+import { ThemeProvider } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from './theme/index';
+
+import NavBar from './components/NavBar/NavBar';
+import Title from './components/Title/Title';
+import Presentation from './components/Presentation/Presentation';
+import Skills from './components/Skills/Skills';
 
 function App() {
   return (
-    <>
-      {/* Aca van los diferentes componentes */}
-      {/* <NavBar /> */}
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar /> 
+      <Title /> 
+      <Presentation /> 
+      <Skills /> 
+    </ThemeProvider>
   );
 };
 
