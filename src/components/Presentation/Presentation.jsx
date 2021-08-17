a1a671c2e376d56ffd8f0f3c792fa3460b8bae2f
Fed-Gar <garcia.fed3rico@gmail.com>
2021-08-17 16:32:01 -0300

    Presentation

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -12,16 +12,20 @@ export default function Presentation() {
   const classes = useStyles();
 
 	return (
-		<Box className={ classes.root }>
+		<Box>
 			<Grid>
 				<Typography variant='h3' className={ classes.title }>
 					Quíen soy?
 				</Typography>
-				<Grid>
+				<Grid className={ classes.paragraph }>
 					<Typography className={ classes.text }>
 						Soy un Desarrollador Web Full-Stack enfocado en el Fron-End.
+					</Typography>
+					<Typography className={ classes.text }>
 						Disfruto del trabajo en equipo y poder crecer gracias al mismo aprendiendo cosas
 						nuevas en el día a día.
+					</Typography>
+					<Typography className={ classes.text }>
 						Alcualmente me encuentro en la búsqueda de un lugar en donde pueda seguir desarrollandome
 						en un ambiente de trabajo colaborativo. 
 					</Typography>
--- src/components/Presentation/styles.js
+++ src/components/Presentation/styles.js
@@ -1,22 +1,17 @@
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
 export const useStyles = makeStyles((theme) => ({
-	root: {
-		width: '100vw',
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'center',
-	},
   title: {
     marginLeft: '4rem',
 		marginBottom: '1rem',
   },
-  text: {
+	paragraph: {
 		width: 1000,
 		height: 'auto',
-		position: 'relative',
 		marginLeft: 'auto',
 		marginRight: 'auto',
+	},
+  text: {
 		fontSize: '1.6rem',
   },
 }));
