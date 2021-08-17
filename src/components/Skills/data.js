a4346f7ede593ca2f27c63e7dfa2cb9d677af81a
Fed-Gar <garcia.fed3rico@gmail.com>
2021-08-17 16:32:13 -0300

    data

---
+++ src/components/Skills/data.js
@@ -0,0 +1,58 @@
+import HTML from '../../assets/file_type_html_icon_130541.png';
+import CSS from '../../assets/file_type_css_icon_130661.png';
+import JavaScript from '../../assets/javascript_icon_130900.png';
+import SASS from '../../assets/file_type_sass_icon_130182.png';
+import Material from '../../assets/material-ui.png';
+import React from '../../assets/react_original_logo_icon_146374.png';
+import Redux from '../../assets/redux_original_logo_icon_146365.png';
+import Node from '../../assets/590px-Node.js_logo.svg.png';
+import Sequelize from '../../assets/sequelize_original_logo_icon_146348.png';
+import PostgreSQL from '../../assets/512px-Postgresql_elephant.svg.png';
+import Git from '../../assets/file_type_git_icon_130581.png';
+
+export const skills = [
+  {
+    logo: HTML,
+    title: 'HTML',
+  },
+  {
+    logo: CSS,
+    title: 'CSS',
+  },
+  {
+    logo: JavaScript,
+    title: 'JavaScript',
+  },
+  {
+    logo: SASS,
+    title: 'SASS',
+  },
+  {
+    logo: Material,
+    title: 'Material UI',
+  },
+  {
+    logo: React,
+    title: 'React',
+  },
+  {
+    logo: Redux,
+    title: 'Redux',
+  },
+  {
+    logo: Node,
+    title: 'Node JS',
+  },
+  {
+    logo: Sequelize,
+    title: 'Sequelize',
+  },
+  {
+    logo: PostgreSQL,
+    title: 'PostgreSQL',
+  },
+  {
+    logo: Git,
+    title: 'Git',
+  },
+];
