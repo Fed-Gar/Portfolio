e0775492575f87f00c02df44f8bda8834494686f
Fede García <garcia.fed3rico@gmail.com>
2021-08-19 11:21:49 -0300

    Footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,14 +3,15 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 export const useStyles = makeStyles((theme) => ({
 	root: {
 		width: '100%',
-		height: 150,
+		height: 100,
 		display:' flex',
 		alignItems: 'center',
 		justifyContent: 'center',
-		marginTop: 100,
-		backgroundColor: theme.palette.background.darkGrey,
+		marginTop: 150,
+		backgroundColor: theme.palette.background.cream,
   },
   text: {
-	fontSize: 15,
+		fontSize: 15,
+		color: theme.palette.text.dark,
   },
 }));
