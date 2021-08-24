997b93518f3cdc150531c50070fd4b4c7bfd065f
Fede García <garcia.fed3rico@gmail.com>
2021-08-24 11:51:16 -0300

    index

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-import store from './store/index';
 import App from './App';
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
